20dffebd200da9d63915b339a6ea793b53af51e1
Anthony Greb <agreb255@gmail.com>
2018-10-24 17:14:46 -0400

    made tests for the components

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import Button from '../components/Button/Button';
+import Button from "../components/Button/Button";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<Button />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("<Button />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Button />, div);
+  });
+  it("should display the button", () => {
+    const wrapper = shallow(<Button />);
+    const display = wrapper.find("button");
+    expect(display.length).toBe(1);
+  });
+  it("should display the component button", () => {
+    const wrapper = shallow(<Button />);
+    const display = wrapper.find("div.component-button");
+    expect(display.length).toBe(1);
   });
 });
